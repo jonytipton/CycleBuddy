6a93e8808307a89b1fe2482422e676bfba6f0261
Jonathan Tipton <nature_explorer98@outlook.com>
2024-03-19 13:23:46 -0700

    Initial Commit

---
+++ CycleBuddy Watch App/CycleBuddyApp.swift
@@ -0,0 +1,17 @@
+//
+//  CycleBuddyApp.swift
+//  CycleBuddy Watch App
+//
+//  Created by Jonathan Tipton on 3/19/24.
+//
+
+import SwiftUI
+
+@main
+struct CycleBuddy_Watch_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ CycleBuddy/CycleBuddyApp.swift
@@ -0,0 +1,17 @@
+//
+//  CycleBuddyApp.swift
+//  CycleBuddy
+//
+//  Created by Jonathan Tipton on 3/19/24.
+//
+
+import SwiftUI
+
+@main
+struct CycleBuddyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
